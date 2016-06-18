72e6aa685354d170bc43ae45a754fee5e7d97a09
Yonatan Bitton <bityon@outlook.com>
2016-06-19 02:08:02 +0300

    Added a lot comments, and improve readability

--- Astar.py
+++ Astar.py
@@ -4,24 +4,31 @@ OBSTACLES_NUM = 300
 X, Y = 30, 30
 
 if __name__ == '__main__':
-	# Create empty grid
+	# Create empty grid, empty grid is '0'
 	grid = [[0 for x in range(X)] for y in range(Y)]
 
-	# Add obstacles in grid
+	# Add obstacles to the grid, represent it in grid with '1'
 	grid = random_obstacles(grid, OBSTACLES_NUM, X, Y)
 	
-	# Random begin point
+	# Random begin point, represent it in grid with 'B'
 	beginx, beginy = random_free_loc(grid, X, Y)
 	grid[beginx][beginy] = "B"
 
-	# Random end point
+	# Random end point, represent it in grid with 'E'
 	endx, endy = random_free_loc(grid, X, Y)
 	grid[endx][endy] = "E"
 
+	# Find the path from begin point to end point
 	p = PathHandler(beginx, beginy, endx, endy, grid, X, Y)
-
 	path = p.find_path()
 
-	grid = draw_path(grid, path, Point(endx, endy))
+	if path == None:
+		print "Didn't find the path. Sorry"
+	else:
+		# Draw all the points on the grid
+		grid = draw_path(grid, path, Point(endx, endy))
 
+	# Print the grid
 	print_grid(grid)
+
+
--- AstarClass.py
+++ AstarClass.py
@@ -17,20 +17,38 @@ class Point:
 		return not self.__eq__(other)
 
 class Node():
-	def __init__(self, x, y, endx, endy, level=0, priority=0):
+	def __init__(self, x, y, endx, endy, move_cost=0):
 		self.x = x
 		self.y = y
-		self.level = level
 		self.endx = endx
 		self.endy = endy
-		self.dist = self.calc_dist()
-		self.priority = priority
+
+		# Distance from this node to  the target node
+		# Also known as h() value
+		self.heuristic = self._calc_h()
+
+		# Movement cost from node to another node
+		# Also known as g() value
+		self.move_cost = move_cost
+
+		# The f() value, combination of heuristic and move values
+		# f() = h() + g()
+		self.priority = 0
+
+		# The parent node, the node before this node in the path
 		self.parent = 0
 
 	def __str__(self):
-		return "Node (x=%d, y=%d, level=%d, dist=%d, priority=%d)" % (self.x, self.y, self.level, self.dist, self.priority)
+		return "Node (x=%d, y=%d, heuristic=%d, move_cost=%d, priority=%d)" % \
+			   (self.x, self.y, self.move_cost, self.heuristic, self.priority)
 
-	def calc_dist(self):
+	def __cmp__(self, other):
+		if other == None or \
+			not isinstance(other, Node):
+			return 1
+		return cmp(self.priority, other.priority)
+
+	def _calc_h(self):
 		xd = self.x - self.endx
 		yd = self.y - self.endy
 		dist = math.fabs(xd) + math.fabs(yd)
@@ -41,16 +59,10 @@ class Node():
 		self.parent = node_parent
 
 	def update_priority(self):
-		self.priority = self.level + self.dist
+		self.priority = self.move_cost + self.heuristic
 
-	def next_level(self, curr_dir):
-		self.level += 10 if (curr_dir % 2 == 0) else 14
-
-	def __cmp__(self, other):
-		if other == None or \
-			not isinstance(other, Node):
-			return 1
-		return cmp(self.priority, other.priority)
+	def update_move_cost(self, curr_dir):
+		self.move_cost += 10 if (curr_dir % 2 == 0) else 14
 
 class PathHandler():
 	def __init__(self, beginx, beginy, endx, endy, grid, X, Y):
@@ -59,56 +71,79 @@ class PathHandler():
 		self.endx = endx
 		self.endy = endy
 		self.grid = grid
-		self.open_list = Q.PriorityQueue()
-		self.close_list = []
 		self.X = X
 		self.Y = Y
 
-	def contains(self, l, filter):
+		# Keep the open nodes in priority queue,
+		# the node with the *lowest* priority will be the first one
+		self.open_list = Q.PriorityQueue()
+
+		# Keep the already visited nodes in list
+		self.close_list = []
+
+	def _contains(self, l, filter):
+		"""Check is list contains element with filter"""
 		for x in l:
 			if filter(x):
 				return True
 		return False
 
-	def contains_node(self, l, x, y):
-		return self.contains(l, lambda node: node.x == x and node.y == y)
+	def _contains_node(self, l, x, y):
+		"""Check is list contains node according to its location"""
+		return self._contains(l, lambda node: node.x == x and node.y == y)
 
-	def get(self, l, filter):
+	def _get(self, l, filter):
+		"""Get first element and index from list if was found"""
 		for i, x in enumerate(l):
 			if filter(x):
 				return i, x
 		return None, None
 
-	def get_node(self, l, x, y):
-		return self.get(l, lambda node: node.x == x and node.y == y)
+	def _get_node(self, l, x, y):
+		"""Get first node and index from list if was found"""
+		return self._get(l, lambda node: node.x == x and node.y == y)
 
-	def search_around(self):
+	def _search_around(self):
+		"""Search arround current node for new nodes in all directions,
+		or better nodes (according to priority) than we already found"""
 		for i in range(len(DIR)):
 			xd = self.currNode.x + DIR[i].x
 			yd = self.currNode.y + DIR[i].y
 
-			if self.is_valid_point(Point(xd, yd)) and \
+			# If this point is valid (in borders), and not occupied by something
+			# and it is not in close list
+			if self._is_valid_point(Point(xd, yd)) and \
 					is_free(self.grid[xd][yd]) and \
-					not self.contains_node(self.close_list, xd, yd):
+					not self._contains_node(self.close_list, xd, yd):
 
-				newNode = Node(xd, yd, self.endx, self.endy, self.currNode.level,
-					self.currNode.priority)
-				newNode.next_level(i)
+				# Create new node with current node move cost and priority
+				newNode = Node(xd, yd, self.endx, self.endy, self.currNode.move_cost)
+				# Update move cost
+				newNode.update_move_cost(i)
+				# Update priority
 				newNode.update_priority()
 
-				if not self.contains_node(self.open_list.queue, xd, yd):
+				# If new node is not in open list, add it to open list
+				# and make the current node its parent
+				if not self._contains_node(self.open_list.queue, xd, yd):
 					self.open_list.put(newNode)
 					newNode.parent = self.currNode
 				else:
-					indexNode, existsNode = self.get_node(self.open_list.queue, xd, yd)
+					# If it is already in open list, and the new node's priority is lower
+					# than the exists one, replace the nodes
+					indexNode, existsNode = self._get_node(self.open_list.queue, xd, yd)
 					if existsNode.priority > newNode.priority:
-						# Remove existsNode
+						# I replace the nodes and not changing the exists node, because the priority queue
+						#  will not be updated after this changing
+
+						# Remove existsNode (directly in the queue attribute which is a list object)
 						del self.open_list.queue[indexNode]
 
 						# Put newNode
 						self.open_list.put(newNode)
 
-	def is_close_to_end(self):
+	def _is_close_to_end(self):
+		"""Chech is current node is close one step to the end"""
 		for d in DIR:
 			if self.currNode.x + d.x == self.endx and \
 				self.currNode.y + d.y == self.endy:
@@ -116,30 +151,7 @@ class PathHandler():
 
 		return False
 
-	def find_path(self):
-		startNode = Node(self.beginx, self.beginy, self.endx, self.endy)
-		startNode.update_priority()
-
-		self.open_list.put(startNode)
-
-		while not self.open_list.empty():
-			self.currNode = self.open_list.get()
-
-			# Check is current node is one step close to end point
-			if self.is_close_to_end():
-				endPoint = Node(self.endx, self.endy, self.endx, self.endx)
-				endPoint.parent = self.currNode
-				self.currNode = endPoint
-
-				return self.return_path()
-
-			self.search_around()
-
-			self.close_list.append(self.currNode)
-
-		return []
-
-	def return_path(self):
+	def _return_path(self):
 		path = []
 
 		while self.currNode.parent != 0:
@@ -148,11 +160,52 @@ class PathHandler():
 
 		return path
 
-	def is_valid_point(self, point):
+	def _is_valid_point(self, point):
 		return point.x >= 0 and \
 				point.x < self.X and \
 				point.y >= 0 and \
 				point.y < self.Y
+
+	def find_path(self):
+		"""Find the path from begin point to end point
+		:return list of Points from end to begin"""
+
+		# Set the start node
+		startNode = Node(self.beginx, self.beginy, self.endx, self.endy)
+		startNode.update_priority()
+
+		# Put the start node in open_list queue
+		self.open_list.put(startNode)
+
+		# While there is still one node in queue,
+		# means we didn't finish to check all points in the grid
+		while not self.open_list.empty():
+			# Get the lowest priority node from queue
+			self.currNode = self.open_list.get()
+
+			# Check is current node is one step close to end point
+			if self._is_close_to_end():
+				# Add endpoint and make its parent the current node
+				endPoint = Node(self.endx, self.endy, self.endx, self.endx)
+				endPoint.parent = self.currNode
+				self.currNode = endPoint
+
+				# Return the full path from end to begin
+				return self._return_path()
+
+			# Search for points arround the current node
+			self._search_around()
+
+			# Add current node to close list (done)
+			self.close_list.append(self.currNode)
+
+		# We didn't found any path,
+		# the path appears to be blocked by obstacles
+		# return None
+		return None
+
+# All directions arround node with points deltas
+DIR = [Point(1, 0), Point(1, 1), Point(0, 1), Point(-1, 1), Point(-1, 0), Point(-1, -1), Point(0, -1), Point(1, -1)]
 
 def is_free(val):
 		return val == 0
@@ -188,4 +241,3 @@ def draw_path(grid, path, end):
 	return grid
 
 
-DIR = [Point(1, 0), Point(1, 1), Point(0, 1), Point(-1, 1), Point(-1, 0), Point(-1, -1), Point(0, -1), Point(1, -1)]
